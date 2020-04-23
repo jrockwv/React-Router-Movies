3de567805ff1d91bde4d2dee8d47728058927020
john ruble <jrockwv@gmail.com>
2020-04-23 00:11:50 -0400

    need to get my onClick to work

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {  Route, Link } from 'react-router-dom'
 import SavedList from './Movies/SavedList';
-
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 const App = () => {
   const [savedList, setSavedList] = useState([]);
   const [movieList, setMovieList] = useState([]);
@@ -28,7 +29,12 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Link to="/MovieList">Movie List</Link>
+        <Route path="/" />
+          <MovieList movies={movieList} />
+        <Route path="/Movies:id" components={Movie} />
+      </div>
     </div>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieList from './MovieList';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;//props.match.params.id
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -18,7 +19,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -33,7 +34,7 @@ const Movie = (props) => {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
+      <div className="movie-card" onClick={MovieList.props.id}>
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
